5ebf99bd62a9599a1e91b53cd9d4e0959093c087
Didarul Islam Laskar <95709968+Imdidarul@users.noreply.github.com>
2026-01-24 21:29:24 +0530

    INS,PUT,DEL added

--- new_sql/utils/db_connection.js
+++ new_sql/utils/db_connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '04082002@Didar',
+    password: '********',
     database: 'testDb'
 })
 
